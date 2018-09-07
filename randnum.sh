6cbfe2e88383953f81346bd2c4ba724af3fe362b
Brendon Bigelow <40188098+americanbigelow@users.noreply.github.com>
2018-09-07 11:46:28 -0500

    Update randnum.sh

--- randnum.sh
+++ randnum.sh
@@ -2,9 +2,9 @@
 
 randnum()
 {
-	local rnvar=$(( $RANDOM%25 )) # pick a number up to 25  
-	echo "$rnvar" # print out that number.
-}
-
+ 	local rnvar=$(( $RANDOM%10 )) 	# Pick a number up to 10.
+	echo "$rnvar"			# This is like a bash version of a return statement. 
+}					# The value of $nvar will be the value of the function
+					# when called as a substitution like this $(randnum).
 
 echo $(randnum)
